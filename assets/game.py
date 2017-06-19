12b4df6d102f06e5dcde1b840fd3f1273630edb7
Articuno1234 <luke.parnell@dbbstu.catholic.edu.au>
2017-06-19 17:45:20 +1000

    Create game.py

---
+++ assets/game.py
@@ -0,0 +1,51 @@
+import os
+import sys
+import random
+import time
+from data import menudata
+def clear_screen():
+    if IS_WINDOWS:
+        os.system("cls")
+    else:
+        os.system("clear")
+
+IS_WINDOWS = os.name == "nt"
+IS_MAC = sys.platform == "darwin"
+INTERACTIVE_MODE = not len(sys.argv) > 1  # CLI flags = non-interactive
+
+def shop():
+     print("Shop\n"
+           "-----------------------------\n")
+
+def game():
+     clear_screen()
+     print(""
+           "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n"
+           "                                                       \n"
+           "                                                       \n"
+           "                                                       \n"
+           "                                                       \n"
+           "                                                       \n"
+           "                                                       \n"
+           "                                                       \n"
+           "                                                       \n"
+           "                                                       \n"
+           "                                                       \n"
+           "                                                       \n"
+           "                                                       \n"
+           "                                                       \n"
+           "                                                       \n"
+           "-------------------------------------------------------\n"
+           " Shop     |  Exit    |                                 \n"
+           "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
+     usr_input = input("> ")
+     while (usr_input != 'Shop' and 'shop') and (usr_input != 'Exit' and 'exit') and (usr_input != '0'):
+         usr_input = input("> ")
+     if usr_input == 'Shop' and 'shop':
+         shop()
+     if usr_input == 'Exit' and 'exit':
+         print("Exiting...")
+         time.sleep(1)
+         clear_screen()
+         print(random.choice(menudata.exit))
+         sys.exit(1)
