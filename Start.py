e364968f3b60f8af43163c00c4f5d20e10068bab
Articuno1234 <luke.parnell@dbbstu.catholic.edu.au>
2017-06-19 13:56:45 +1000

    Update and rename Start.py to start.py

--- Start.py
+++
@@ -1,5 +0,0 @@
-try:
-     import sys
-     import os
- except ImportError:
-         print("sys or os was not found!")
---
+++ start.py
@@ -0,0 +1,12 @@
+try:
+     import sys
+     import os
+ except ImportError:
+         print("sys or os was not found!")
+          
+          
+          
+def menu():
+     print("-=-=-=-=-=-=-=-=-=-=\n"
+           "     Aquarium\n"
+           "-=-=-=-=-=-=-=-=-=-=\n")
